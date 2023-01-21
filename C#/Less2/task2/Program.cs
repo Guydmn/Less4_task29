3a34afe7594b8d4543ea1c8bf4b2639ef22aa9a7
Roman Guydmn <romanguydmn@gmail.com>
2023-01-21 05:57:29 +0400

    Задача2 Семинар2

---
+++ C#/Less2/task2/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.Write("Введите 3х значное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a > 99 && a < 1000)
+{
+    Console.Write($"Ваше новое число {a / 100}");
+    Console.Write(a % 10);
+}
+else
+{
+    Console.Write("Вы ввели неверное число");
+}
