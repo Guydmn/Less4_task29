2403d2b7eb84ccd2e79c09e3f4e075edc05383f6
Roman Guydmn <romanguydmn@gmail.com>
2023-01-21 06:47:17 +0400

    Задача4 Семинар2

---
+++ C#/Less2/task4/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int a = new Random().Next(1, 1001);
+Console.WriteLine($"Наше случайное число {a}");
+int x1 = a % 7;
+int x2 = a % 23;
+
+if (x1 == 0 && x2 == 0)
+    Console.Write("Число одновременно кратно и 7 и 23");
+else
+    Console.Write("Число одновременно не кратно 7 и 23");
