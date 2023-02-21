0f98a1a8774177c4b25375fb15ad56fccc5a8f5a
Roman Guydmn <romanguydmn@gmail.com>
2023-02-21 16:57:40 +0400

    Задача 54 Семинар8 почистил ненужный код

---
+++ C#/Less8/less8_task54/Program.cs
@@ -0,0 +1,70 @@
+﻿
+Console.Write("Введите высоту массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите длинну массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int [,] doubleArray = new int [m, n];
+
+Random rnd = new Random();
+
+for (int i = 0; i < doubleArray.GetLength(0); i++)
+{
+    for (int j = 0; j < doubleArray.GetLength(1); j++)
+    {
+        doubleArray[i, j] = rnd.Next(0, 10);
+        Console.Write($"{doubleArray[i, j]} ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine($"Массив после сортировки каждого ряда: ");
+
+for (int i = 0; i < doubleArray.GetLength(0); i++)
+{
+    for (int j = 0; j < doubleArray.GetLength(1) - 1; j++)
+    {
+        for (int k = 0; k < doubleArray.GetLength(1) - j - 1; k++)
+        {
+            if (doubleArray[i, k] < doubleArray[i, k + 1])
+            {
+                int temp = doubleArray[i, k];
+                doubleArray[i, k] = doubleArray[i, k + 1];
+                doubleArray[i, k + 1] = temp;
+            }
+        }
+    }
+}
+
+for (int i = 0; i < doubleArray.GetLength(0); i++)
+{
+    for (int j = 0; j < doubleArray.GetLength(1); j++)
+    {
+        Console.Write($"{doubleArray[i, j]} ");
+    }
+    Console.WriteLine();
+}
+
+
+
+
+// doubleArray[i, j-1] > max ||
+
+//  max = doubleArray[i, j];
+// doubleArray[i, j] = doubleArray[i, j-1];
+// doubleArray[i, j-1] = max;
+
+// doubleArray[i, j] > doubleArray[i, j-1]
+
+// temp[i, j] = doubleArray[i, j];
+// doubleArray[i, j] = doubleArray[i, j-1];
+// doubleArray[i, j-1] = temp[i, j];
+
+// temp = doubleArray[i,j];
+// doubleArray[i, j] = doubleArray[i, j-1]; 
+// doubleArray[i, j-1] = temp;
+
+//  while (doubleArray[i, j] > max)
+//                 {
+//                     max = doubleArray[i, j];
+//                     doubleArray[i, j] = doubleArray[i, j-1];
+//                     doubleArray[i, j-1] = max;
+//                 }
